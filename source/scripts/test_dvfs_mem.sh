0485f3c43032dced77ecfe999afe739b14a159f4
Willy Wolff <willy.mh.wolff@gmail.com>
2020-06-24 09:08:41 +0200

    source/scripts/test_dvfs_mem.sh: print out dvfs memory bus trans_stat

--- source/scripts/test_dvfs_mem.sh
+++ source/scripts/test_dvfs_mem.sh
@@ -15,11 +15,15 @@ mem_freq=`cat /sys/class/devfreq/10c20000.memory-controller/cur_freq`
 
 printf "\n\nmem_gov = ${mem_gov} at ${mem_freq}\n\n"
 
+cat /sys/class/devfreq/10c20000.memory-controller/trans_stat
+
 printf "\n\n Running on the LITTLE cluster\n\n"
 taskset 8 ./bench_install/bin/microbe_cache 33554431 0 9722222 1 ${little_freq}
 
 printf "\n\n Running on the big cluster\n\n"
 taskset 80 ./bench_install/bin/microbe_cache 33554431 0 9722222 1 ${big_freq}
+
+cat /sys/class/devfreq/10c20000.memory-controller/trans_stat
 
 sudo bash -c "echo performance > /sys/class/devfreq/10c20000.memory-controller/governor"
 
@@ -28,11 +32,15 @@ mem_freq=`cat /sys/class/devfreq/10c20000.memory-controller/cur_freq`
 
 printf "\n\nmem_gov = ${mem_gov} at ${mem_freq}\n\n"
 
+cat /sys/class/devfreq/10c20000.memory-controller/trans_stat
+
 printf "\n\n Running on the LITTLE cluster\n\n"
 taskset 8 ./bench_install/bin/microbe_cache 33554431 0 9722222 1 ${little_freq}
 
 printf "\n\n Running on the big cluster\n\n"
 taskset 80 ./bench_install/bin/microbe_cache 33554431 0 9722222 1 ${big_freq}
+
+cat /sys/class/devfreq/10c20000.memory-controller/trans_stat
 
 sudo bash -c "echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
 sudo bash -c "echo ondemand > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor"
